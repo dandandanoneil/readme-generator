d9de102d0667a9989f2d595999f07323ad44a4e1
Dan ONeil <dhoneil@gmail.com>
2020-10-28 12:20:16 -0400

    Added template to writeToFile() function, deleted old test file that accidentally got committed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer')
 
-// array of questions for user
-const questions = [
+// array of prompts for user
+const prompts = [
     'GitHub Username:',
     'Contact Email:',
     'Project Title:',
@@ -17,35 +17,34 @@ const questions = [
 // function to write README file
 function writeToFile(fileName, data) {
     // Code here that takes the data and makes it way prettier
-
-    // But as a placeholder:
-    let parsedData = JSON.stringify(data);
+    let parsedData = `# ${data.title}\n${data.description}\n## Table of Contents\n- [Installation Steps](#installation-steps)\n- [Usage Instructions](#usage-instructions)\n- [Licenses](#licenses)\n- [Contribution Guidelines](#contribution-guidelines)\n- [Testing Instructions](#testing-instructions)\n---\n## Installation Steps\n${data.installation}\n## Usage Instructions\n${data.usage}\n## Licenses\n...BADGE PLACEHOLDER\n## Contribution Guidelines\n${data.contributions}\n## Testing Instructions\n${data.testing}\n\n---\nGenerated using [README Generator](https://github.com/dandandanoneil/readme-generator)`
 
     fs.writeFile(fileName, parsedData , err => { 
         if (err) 
           console.log(err); 
         else { 
-          console.log('File written successfully');
+          console.log('File created successfully!');
         } 
     }); 
 }
 
 // function to initialize program
 function init() {
-    console.log('Let\'s generate a professional README.md for your open source GithHub project!\nEnter your project info to add it to the README...')
+    console.log(
+        'Let\'s generate a professional README.md for your open source GithHub project!\nEnter your project info to add it to the README...')
     inquirer.prompt([
-        {type: 'input', message: questions[0], name: 'github'}, 
-        {type: 'input', message: questions[1], name: 'email'},
-        {type: 'input', message: questions[2], name: 'title'},
-        {type: 'input', message: questions[3], name: 'description'},
-        {type: 'input', message: questions[4], name: 'installation'},
-        {type: 'input', message: questions[5], name: 'usage'},
-        {type: 'list', message: questions[6], name: 'license', choices: ['MIT', 'GNU']},
-        {type: 'input', message: questions[7], name: 'contributions'},
-        {type: 'input', message: questions[8], name: 'testing'}
+        {type: 'input', message: prompts[0], name: 'github'}, 
+        {type: 'input', message: prompts[1], name: 'email'},
+        {type: 'input', message: prompts[2], name: 'title'},
+        {type: 'input', message: prompts[3], name: 'description'},
+        {type: 'input', message: prompts[4], name: 'installation'},
+        {type: 'input', message: prompts[5], name: 'usage'},
+        {type: 'list', message: prompts[6], name: 'license', choices: ['MIT', 'GNU']},
+        {type: 'input', message: prompts[7], name: 'contributions'},
+        {type: 'input', message: prompts[8], name: 'testing'}
     ])
     .then(answers => {
-        let fileName = answers.title.split(' ').join('-') + '.md';
+        let fileName = answers.title.split(' ').join('-') + '-README.md';
         writeToFile(fileName, answers);
     })
     .catch(err => console.log(err));
